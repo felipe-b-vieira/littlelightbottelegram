35ba5bdac28d9109d0ad395d1f0e1753aae6c089
felgal <rag.felipe@gmail.com>
2020-01-30 20:37:13 -0300

    Inicio conexão mongodb com mongoengine

--- main.py
+++ main.py
@@ -53,7 +53,6 @@ def conectaDB():
 	
 class PostTeste(Document):
     title = StringField(required=True, max_length=200)
-    posted = DateTimeField(default=datetime.datetime.utcnow)
     tags = ListField(StringField(max_length=50))
     meta = {'allow_inheritance': True}
 
