0ab74becf33dd291fe5c4188d53be515645bf782
felgal <rag.felipe@gmail.com>
2020-02-05 00:52:46 -0300

    Salva usuário no mongodb

--- controleUsuarios/controle_de_usuarios.py
+++ controleUsuarios/controle_de_usuarios.py
@@ -3,13 +3,15 @@ from mongoengine import *
 	
 class Usuario(Document):
     id = IntField(required=True, max_length=200)
-    is_bot = StringField(required=True, max_length=200)
+    is_bot = BooleanField(required=True, default = False)
     username = StringField( max_length=200)
     full_name = StringField(required=True, max_length=200)
 
 #função responsável por salvar o usuário no banco de dados(AINDA SEM PROTEÇÂO DE QUANTIDADE)
 def salva_usuario(update, context):
-	usuarioAtual = update.message.from_user()
+	console.log(update.message.from_user)
+	console.log(update.message.from_user())
+	usuarioAtual = update.message.from_user
 	
 	usuarioDB = Usuario(id=usuarioAtual.id)
 	usuarioDB.is_bot = usuarioAtual.is_bot
