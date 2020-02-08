e9c366cc90b91ba61782eb8be84ff268b4d1ffe2
felgal <rag.felipe@gmail.com>
2020-02-07 22:04:50 -0300

    Salvando usuários únicos

--- controleUsuarios/controle_de_usuarios.py
+++ controleUsuarios/controle_de_usuarios.py
@@ -1,8 +1,8 @@
 from mongoengine import *
 
 	
-class Usuario(Document):
-    idTelegram = IntField(required=True, max_length=200)
+class Usuarios(Document):
+    idTelegram = IntField(required=True, max_length=200, primary_key=True)
     is_bot = BooleanField(required=True, default = False)
     username = StringField( max_length=200)
     full_name = StringField(required=True, max_length=200)
@@ -10,8 +10,9 @@ class Usuario(Document):
 #função responsável por salvar o usuário no banco de dados(AINDA SEM PROTEÇÂO DE QUANTIDADE)
 def salva_usuario(update, context):
 	usuarioAtual = update.message.from_user
-	usuarioDB = Usuario(idTelegram =usuarioAtual.id)
-	usuarioDB.is_bot = usuarioAtual.is_bot
-	usuarioDB.username = usuarioAtual.username
-	usuarioDB.full_name = usuarioAtual.full_name
-	usuarioDB.save()
+	if not Usuarios.objects(idTelegram=usuarioAtual.id):
+		usuarioDB = Usuarios(idTelegram =usuarioAtual.id)
+		usuarioDB.is_bot = usuarioAtual.is_bot
+		usuarioDB.username = usuarioAtual.username
+		usuarioDB.full_name = usuarioAtual.full_name
+		usuarioDB.save()
