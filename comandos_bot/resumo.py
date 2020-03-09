57cd25f1bf5eb6937548d2c833172f6b9e701970
felgal <rag.felipe@gmail.com>
2020-03-09 19:02:01 -0300

    Função de resumo feita

--- comandos_bot/resumo.py
+++ comandos_bot/resumo.py
@@ -13,16 +13,11 @@ algo = client.algo('nlp/Summarizer/0.1.3')
 #função que resume a mensagem enviada
 def resumo(update, context):
 	 try:
-
-        # pega o texto que o usuário mandou
-         text = update.message.text
-
-        # sumariza usando o Algorithmia
-         summary = algo.pipe(text)
-
-        # faz o bot enviar o resultado
-         bot.sendMessage(chat_id=update.message.chat_id,text=summary.result)
-
-    except UnicodeEncodeError:
-
-        bot.sendMessage(chat_id=update.message.chat_id,text="I'm sorry Dave, I'm afraid I can't do that")
+		# pega o texto que o usuário mandou
+		text = update.message.text
+		# sumariza usando o Algorithmia
+		summary = algo.pipe(text)
+		# faz o bot enviar o resultado
+		bot.sendMessage(chat_id=update.message.chat_id,text=summary.result)
+	except UnicodeEncodeError:
+		bot.sendMessage(chat_id=update.message.chat_id,text="I'm sorry Dave, I'm afraid I can't do that")
